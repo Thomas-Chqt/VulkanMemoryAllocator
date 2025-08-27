6ca623a4e07ecac689fb456148c065e415502ae2
Thomas Choquet <semoir.dense-0h@icloud.com>
2025-08-27 21:48:51 +0900

    add cpp file

---
+++ src/vk_mem_alloc.cpp
@@ -0,0 +1,11 @@
+/*
+ * ---------------------------------------------------
+ * vk_mem_alloc.cpp
+ *
+ * Author: Thomas Choquet <semoir.dense-0h@icloud.com>
+ * Date: 2025/08/27 21:44:11
+ * ---------------------------------------------------
+ */
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
